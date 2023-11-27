7c59b45ae32577332225dbc4b6116f698611265d
mykolamateichuk <mateichukmykola@gmail.com>
2023-11-27 10:56:50 +0200

    Add todo_list app and models implementation

--- todo_list_core/settings.py
+++ todo_list_core/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#AUTH_USER_MODEL = "todo_list.User"
+# AUTH_USER_MODEL = "todo_list.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
